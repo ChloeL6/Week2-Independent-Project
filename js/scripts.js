b33fa6a651f78af91ea513e32f4f9308c9afb677
Chloe Le <yenchi.le1199@gmail.com>
2022-10-21 12:04:56 -0700

    Add hidden form and result functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
 function getData(event) {
-  event.prevenDefault();
+  event.preventDefault();
   const activities = document.getElementById("activities").value;
   const flavor = document.getElementById("flavor").value;
   const learn = document.getElementById("learn").value;
   const code = document.querySelector("input[name='code']:checked").value;
   const dog = document.querySelector("input[name='dog']:checked").value;
+
+let result = "Your language is ";
+if (code === "no" || flavor === "vanila") {
+  result = result + "Ruby";
+} else if (activities === "Practice coding" && learn === "It's fun"){
+  result = result + "Python";
+} else if (code === "Yes" || dog === "Absolutely") {
+  result = result + "JavaScript";
 }
+
+document.getElementById("mainOutput").removeAttribute("class");
+
+document.getElementById("questions").classList.add("hidden");
+
+document.getElementById("outPut").innerText = result;
+}
+
 window.addEventListener("load", function() {
   document.getElementById("questions").addEventListener("submit", getData);
 });
-
-window.addEventListener("load", function(){
-  
-}
