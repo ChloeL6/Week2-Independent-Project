693aab111723120c89ad9ccb5d587021c30f5ee8
Chloe Le <yenchi.le1199@gmail.com>
2022-10-30 12:18:34 -0700

    Successfully seperate UI and BI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,21 @@
-// function getData(event) {
-//   event.preventDefault();
-//   const activities = document.getElementById("activities").value;
-//   const flavor = document.getElementById("flavor").value;
-//   const learn = document.getElementById("learn").value;
-//   const code = document.querySelector("input[name='code']:checked").value;
-//   const dog = document.querySelector("input[name='dog']:checked").value;
-
-// let result = "The first program language you should learn is ";
-// if (code === "no" || flavor === "vanila" || learn === "It's challenging") {
-//   result = result + "Ruby.";
-// } else if (activities === "Practice coding" && learn === "It's fun"){
-//   result = result + "Python.";
-// } else if (code === "Yes" || dog === "Absolutely") {
-//   result = result + "JavaScript.";
-// } else {
-//   result = "You can learn whichever you want for now!";
-// }
-
-// document.getElementById("mainOutput").removeAttribute("class");
-// document.getElementById("questions").classList.add("hidden");
-
-// document.getElementById("outPut").innerText = result;
-// }
-
-// function goBackButton() {
-//   document.getElementById("questions").removeAttribute("class");
-//   document.getElementById("mainOutput").classList.add("hidden");
-// }
-
-// window.addEventListener("load", function() {
-//   document.getElementById("questions").addEventListener("submit", getData);
-// });
-
-// window.addEventListener("load", function() {
-//   document.getElementById("backButton").addEventListener("click", goBackButton);
-// });
 
 // BI Logic
 
 function languageSuggester(activities, flavor, learn, code, dog) {
 
-let result = "The first program language you should learn is ";
+  let result = "The first program language you should learn is ";
+  
+  if (code === "no" || flavor === "vanila" || learn === "It's challenging") {
+    return result = result + "Ruby.";
+  } else if (activities === "Practice coding" && learn === "It's fun"){
+    return result = result + "Python.";
+  } else if (code === "Yes" || dog === "Absolutely") {
+    return result = result + "JavaScript.";
+  } else {
+    return result = "You can learn whichever you want for now!";
+  }
+}
 
-if (code === "no" || flavor === "vanila" || learn === "It's challenging") {
-  result = result + "Ruby.";
-} else if (activities === "Practice coding" && learn === "It's fun"){
-  result = result + "Python.";
-} else if (code === "Yes" || dog === "Absolutely") {
-  result = result + "JavaScript.";
-} else {
-  result = "You can learn whichever you want for now!";
-}
-}
 
 //UI logic
 
