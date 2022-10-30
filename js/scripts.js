072e4e837af70abdb98823596e3f151355474f56
Chloe Le <yenchi.le1199@gmail.com>
2022-10-30 11:44:56 -0700

    Trying to seperate UI and BI logic''

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,47 @@
-function getData(event) {
-  event.preventDefault();
-  const activities = document.getElementById("activities");
-  const flavor = document.getElementById("flavor").value;
-  const learn = document.getElementById("learn").value;
-  const code = document.querySelector("input[name='code']:checked").value;
-  const dog = document.querySelector("input[name='dog']:checked").value;
+// function getData(event) {
+//   event.preventDefault();
+//   const activities = document.getElementById("activities").value;
+//   const flavor = document.getElementById("flavor").value;
+//   const learn = document.getElementById("learn").value;
+//   const code = document.querySelector("input[name='code']:checked").value;
+//   const dog = document.querySelector("input[name='dog']:checked").value;
+
+// let result = "The first program language you should learn is ";
+// if (code === "no" || flavor === "vanila" || learn === "It's challenging") {
+//   result = result + "Ruby.";
+// } else if (activities === "Practice coding" && learn === "It's fun"){
+//   result = result + "Python.";
+// } else if (code === "Yes" || dog === "Absolutely") {
+//   result = result + "JavaScript.";
+// } else {
+//   result = "You can learn whichever you want for now!";
+// }
+
+// document.getElementById("mainOutput").removeAttribute("class");
+// document.getElementById("questions").classList.add("hidden");
+
+// document.getElementById("outPut").innerText = result;
+// }
+
+// function goBackButton() {
+//   document.getElementById("questions").removeAttribute("class");
+//   document.getElementById("mainOutput").classList.add("hidden");
+// }
+
+// window.addEventListener("load", function() {
+//   document.getElementById("questions").addEventListener("submit", getData);
+// });
+
+// window.addEventListener("load", function() {
+//   document.getElementById("backButton").addEventListener("click", goBackButton);
+// });
+
+// BI Logic
+
+function languageSuggester(activities, flavor, learn, code, dog) {
 
 let result = "The first program language you should learn is ";
+
 if (code === "no" || flavor === "vanila" || learn === "It's challenging") {
   result = result + "Ruby.";
 } else if (activities === "Practice coding" && learn === "It's fun"){
@@ -16,11 +51,29 @@ if (code === "no" || flavor === "vanila" || learn === "It's challenging") {
 } else {
   result = "You can learn whichever you want for now!";
 }
+}
+
+//UI logic
+
+function getData(event) {
+  event.preventDefault();
+
+  const activities = document.getElementById("activities").value;
+  const flavor = document.getElementById("flavor").value;
+  const learn = document.getElementById("learn").value;
+  const code = document.querySelector("input[name='code']:checked").value;
+  const dog = document.querySelector("input[name='dog']:checked").value;
+console.log("act: " + activities); // got data from the form
+console.log("act: " + flavor);
+
+
+let finalResult = languageSuggester(activities, flavor, learn, code, dog);
 
 document.getElementById("mainOutput").removeAttribute("class");
 document.getElementById("questions").classList.add("hidden");
 
-document.getElementById("outPut").innerText = result;
+document.getElementById("outPut").innerText = finalResult;
+console.log(finalResult);
 }
 
 function goBackButton() {
